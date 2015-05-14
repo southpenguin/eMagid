d5d82757f10ac07b6b7edb571916fe3053d7a58a
southpenguin <southpenguin@gmail.com>
2015-05-14 06:47:27 -0400

    Create connecDB.php

---
+++ connecDB.php
@@ -0,0 +1,9 @@
+<?php
+    $hostname = ""; //deleted
+    $username = ""; //deleted
+    $password = ""; //deleted
+    $database = "emagid";
+    $port = 3306;
+    
+    $db = new mysqli($hostname,$username,$password,$database,$port);
+?>
