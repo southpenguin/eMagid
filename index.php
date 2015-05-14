fb941800209079cf000c960678cfc973bdb71777
southpenguin <southpenguin@gmail.com>
2015-05-14 06:44:01 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'management.php';?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>eMagid Management</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+</head>
+<body>
+    <ol class="tree">
+        <?php $cat->listCat();?>
+    </ol>
+    
+    
+                
+</body>
+</html>
